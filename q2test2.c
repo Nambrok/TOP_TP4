333312556fc6ccf5558a818c38993f3d94297239
Damien Thenot <spaceship84@gmail.com>
2018-04-04 18:18:47 +0200

    Fin question 2

--- q2test2.c
+++ q2test2.c
@@ -21,6 +21,20 @@ int_bmp_pixel_t * transformerOneDim(int_bmp_pixel_t ** tab, int height, int widt
         return tabO;
 }
 
+int_bmp_pixel_t * transformerOneDimTransposed(int_bmp_pixel_t ** tab, int height, int width){
+        //Renvoi un tableau d'une dimension de tab
+        int_bmp_pixel_t * tabO = calloc(height * width, sizeof(int_bmp_pixel_t));
+        int i, j;
+
+        for(i = 0; i<width; i++) {
+                for(j = 0; j<height; j++) {
+                        tabO[(i * width) + j] = tab[j][i];
+                }
+        }
+
+        return tabO;
+}
+
 void transformerDeuxDim(int_bmp_pixel_t ** tab, int_bmp_pixel_t * rbuf, int height, int width){
         int i, j;
         for(i = 0; i<height; i++) {
@@ -30,16 +44,23 @@ void transformerDeuxDim(int_bmp_pixel_t ** tab, int_bmp_pixel_t * rbuf, int heig
         }
 }
 
-int main(int argc, char* argv[])
-{
-        MPI_Init(&argc, &argv);
+//Attention transposé marche ici car la matrice est carré
+
+void transformerDeuxDimTransposed(int_bmp_pixel_t ** tab, int_bmp_pixel_t * rbuf, int height, int width){
+        int i, j;
+        for(i = 0; i<width; i++) {
+                for(j = 0; j<height; j++) {
+                        tab[j][i] = rbuf[i * width + j];
+                }
+        }
+}
+
+void miroirVertical(char * file){
         int nproc, rank;
         MPI_Comm_size(MPI_COMM_WORLD, &nproc);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
         int i, j;
-
-
         int height, width;
         int_bmp_pixel_t ** tab = NULL;
         int_bmp_pixel_t * tabOneDim = NULL;
@@ -61,7 +82,7 @@ int main(int argc, char* argv[])
 
 
         if(rank == ROOT) {
-                tab = Lecture_image("pingouin.bmp");
+                tab = Lecture_image(file);
                 height = get_img_heigh();
                 width = get_img_width();
                 tabOneDim = transformerOneDim(tab, height, width);
@@ -74,7 +95,7 @@ int main(int argc, char* argv[])
 
         int heightLoc = height / nproc;
 
-        int_bmp_pixel_t * tabLocal = malloc(heightLoc * width * sizeof(int_bmp_pixel_t));
+
 
         int * displs, * counts;
         counts = calloc(nproc, sizeof(int));
@@ -83,14 +104,16 @@ int main(int argc, char* argv[])
                 displs[i] = i * heightLoc * width;
                 counts[i] = heightLoc * width;
         }
+        counts[i-1] += height%nproc * width;
+
+        int_bmp_pixel_t * tabLocal = malloc(counts[rank] * sizeof(int_bmp_pixel_t));
 
         MPI_Scatterv(tabOneDim, counts, displs, mpi_pixel_type, tabLocal, counts[rank], mpi_pixel_type, ROOT, MPI_COMM_WORLD);
 
         //Miroir Vertical
-        for(i = 0; i < heightLoc; i++)
-        {
-                for(j = 0; j < width/2; j++)
-                {
+        int heightSelf = counts[rank] / width;
+        for(i = 0; i < heightSelf; i++) {
+                for(j = 0; j < width/2; j++) {
                         tmp = tabLocal[(i * width) + (width - j - 1)];
                         tabLocal[(i * width) + (width - j - 1)] = tabLocal[(i * width) + j];
                         tabLocal[i * width + j] = tmp;
@@ -110,6 +133,98 @@ int main(int argc, char* argv[])
                 Ecriture_image(tab, "copie.bmp");
                 Liberation_image_lue(tab);
         }
+}
+
+void miroirHorizontal(char * file){
+        int nproc, rank;
+        MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        int i, j;
+        int height, width;
+        int_bmp_pixel_t ** tab = NULL;
+        int_bmp_pixel_t * tabOneDim = NULL;
+
+        int_bmp_pixel_t tmp;
+
+        /* Création du type MPI pour un int_bmp_pixel_t*/
+        int blocklengths[3] = {1, 1, 1};
+        MPI_Datatype types[3] = {MPI_INT, MPI_INT, MPI_INT};
+        MPI_Datatype mpi_pixel_type;
+        MPI_Aint offsets[3];
+
+        offsets[0] = offsetof(int_bmp_pixel_t, Rouge);
+        offsets[1] = offsetof(int_bmp_pixel_t, Bleu);
+        offsets[2] = offsetof(int_bmp_pixel_t, Vert);
+
+        MPI_Type_create_struct(3, blocklengths, offsets, types, &mpi_pixel_type);
+        MPI_Type_commit(&mpi_pixel_type);
+
+
+        if(rank == ROOT) {
+                tab = Lecture_image(file);
+                height = get_img_heigh();
+                width = get_img_width();
+                tabOneDim = transformerOneDimTransposed(tab, height, width);
+        }
+
+
+
+        MPI_Bcast(&height, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+        MPI_Bcast(&width, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+
+        int heightLoc = height / nproc;
+
+
+
+        int * displs, * counts;
+        counts = calloc(nproc, sizeof(int));
+        displs = calloc(nproc, sizeof(int));
+        for(i = 0; i<nproc; i++) {
+                displs[i] = i * heightLoc * width;
+                counts[i] = heightLoc * width;
+        }
+        counts[i-1] += height%nproc * width;
+
+        int_bmp_pixel_t * tabLocal = malloc(counts[rank] * sizeof(int_bmp_pixel_t));
+
+        MPI_Scatterv(tabOneDim, counts, displs, mpi_pixel_type, tabLocal, counts[rank], mpi_pixel_type, ROOT, MPI_COMM_WORLD);
+
+        //Miroir Vertical
+        int heightSelf = counts[rank] / width;
+        for(i = 0; i < heightSelf; i++) {
+                for(j = 0; j < width/2; j++) {
+                        tmp = tabLocal[(i * width) + (width - j - 1)];
+                        tabLocal[(i * width) + (width - j - 1)] = tabLocal[(i * width) + j];
+                        tabLocal[i * width + j] = tmp;
+                }
+        }
+
+        int_bmp_pixel_t * rbuf = NULL;
+
+
+        rbuf = calloc(height * width, sizeof(int_bmp_pixel_t));
+
+
+        MPI_Gatherv(tabLocal, counts[rank], mpi_pixel_type, rbuf, counts, displs, mpi_pixel_type, ROOT, MPI_COMM_WORLD);
+
+        if(rank == ROOT) {
+                transformerDeuxDimTransposed(tab, rbuf, height, width);
+                Ecriture_image(tab, "copie.bmp");
+                Liberation_image_lue(tab);
+        }
+}
+
+int main(int argc, char* argv[])
+{
+        MPI_Init(&argc, &argv);
+        int nproc, rank;
+        MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+        miroirVertical("pingouin.bmp");
+        miroirHorizontal("pingouin.bmp");
+
 
 
         MPI_Finalize();
